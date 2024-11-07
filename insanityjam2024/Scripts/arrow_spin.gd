c20975ab6d74ee814d62397d1d05c8856c3a5189
Adri0211 <adrios.0211@gmail.com>
2024-11-07 20:11:41 +0100

    Proto
    Arrow and Area

--- insanityjam2024/Scripts/arrow_spin.gd
+++ insanityjam2024/Scripts/arrow_spin.gd
@@ -1,11 +1,31 @@
 extends Sprite2D
 
+const ROTATION_SPEED = 0.001
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+enum { ROTATING, CALCULATING}
 
+@onready var target: Sprite2D = $Target
+
+var state = CALCULATING
+var direction = 1
+var speed = 0
+var time = 0
+var difficulty = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Check State and set values
+	if state == ROTATING:
+		rotate(ROTATION_SPEED*direction*speed*difficulty)
+		time -= delta
+		if time < 0:
+			state = CALCULATING
+	elif state == CALCULATING:
+		direction = roundi(randf())*2-1
+		speed = randf()
+		time = randf()*2+1 * speed
+		state = ROTATING
+		difficulty += 0.5
+	
+	# Move target
+	target.position.x = get_global_mouse_position().distance_to(position)
