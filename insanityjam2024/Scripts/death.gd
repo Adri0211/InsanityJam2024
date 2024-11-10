dc542cf8da05e83d757d92b89024104e83689f31
Ferran <158285092+MrMayonnais@users.noreply.github.com>
2024-11-10 02:57:28 +0100

    fuckfuck

--- insanityjam2024/Scripts/death.gd
+++ insanityjam2024/Scripts/death.gd
@@ -1,5 +1,8 @@
 extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _on_mouse_entered() -> void:
+	
+	animated_sprite_2d.play("explode")
 	get_tree().root.get_node("Game").get_node("GameManager")._player_died()
-	print("dead")
+	
